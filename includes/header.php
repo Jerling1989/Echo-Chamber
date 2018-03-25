b0934258e9051319d3dc1cfef41778cf11cc85b0
Jacob <jerling1989@gmail.com>
2018-03-25 03:17:58 -0400

    nav bar icons

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,14 @@
 
 	// CHECK IF USER IS SIGNED IN
 	if(isset($_SESSION['username'])) {
+		// CREATE VARIABLE FOR USERNAME
 		$userLoggedIn = $_SESSION['username'];
+
+		// QUERY TO FIND USER DETAILS
+		$user_details_query = mysqli_query($connection, "SELECT * FROM users WHERE username='$userLoggedIn'");
+		// STORE USER DETAILS INTO ARRAY
+		$user = mysqli_fetch_array($user_details_query);
+
 		// IF NOT SIGNED IN REDIRECT USER TO LOGIN PAGE
 	} else {
 		header('Location: register.php');
@@ -23,6 +30,9 @@
   <meta charset="utf-8" />
   <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <!-- FONT AWESOME LINKS -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css" integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P" crossorigin="anonymous">
   <!-- RESET CSS LINK -->
   <link rel="stylesheet" type="text/css" href="assets/css/reset.css" />
 	<!-- BOOTSTRAP CSS LINK -->
@@ -44,9 +54,37 @@
 <body>
 
 	<div id="top-bar">
+
 		<div id="logo">
-			<a href="index.php">ECHO CHAMBER</a>
+			<a href="index.php">Swirlfeed!</a>
 		</div>
+
+		<nav>
+			<a href="#">
+				<?php
+					echo $user['first_name'];
+				?>
+			</a>
+			<a href="index.php">
+				<i class="fas fa-home fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fas fa-envelope fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fas fa-bell fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fas fa-users fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fas fa-cog fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fas fa-sign-out-alt fa-lg"></i>
+			</a>
+		</nav>
+
 	</div>
 
 
