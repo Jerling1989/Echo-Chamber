51f628474b6f75b9eefc5b5ee8b249f44a6b5752
Jacob <jerling1989@gmail.com>
2018-03-25 21:35:15 -0400

    user class comments

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -1,19 +1,29 @@
 <?php
 
 	class User {
+		// PRIVATE VARIABLES
 		private $user;
 		private $connection;
 
+		// CREATE PUBLIC VARIABLES
 		public function __construct($connection. $user) {
+			// CONNECTION VARIABLE
 			$this->connection = $connection;
+			// DATABASE QUERY
 			$user_details_query = mysqli_query($connection, "SELECT * FROM users WHERE username='$user'");
+			// QUERY RESULT ARRAY
 			$this->user = mysqli_fetch_array($user_details_query);
 		}
 
+		// GET FIRST AND LAST NAME FUNCTION
 		public function getFirstAndLastName() {
+			// USERNAME VARIABLE
 			$username = $this->user['username'];
+			// DATABASE QUERY (FIRST AND LAST NAME)
 			$query = mysqli_query($this->connection, "SELECT first_name, last_name FROM users WHERE username='$username'");
+			// STORE FIRST AND LAST NAME IN ARRAY
 			$row = mysqli_fetch_array($query);
+			// RETURN FIRST AND LAST NAME
 			return $row['first_name'] . ' ' . $row['last_name'];
 		}
 
