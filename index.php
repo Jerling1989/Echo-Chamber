dac46c86d39a043a3602b52fe439339d9cc47670
Jacob <jerling1989@gmail.com>
2018-03-25 20:08:57 -0400

    creating newsfeed

--- index.php
+++ index.php
@@ -3,8 +3,37 @@
 	// session_destroy();
 ?>
 
-	<div class="user_details column">
-		<a href="#"><img src="<?php echo $user['profile_pic']; ?>" /></a>
+	<div class="user-details column">
+
+		<a href="#">
+			<img src="<?php echo $user['profile_pic']; ?>" />
+		</a>
+
+		<div class="user-details-left-right">
+			<a href="#">
+				<?php
+					echo $user['first_name'] . ' ' . $user['last_name'];
+				?>
+			</a>
+			<br />
+			<?php
+				echo 'Posts: ' . $user['num_posts'] . '<br />';
+				echo 'Likes: ' . $user['num_likes'];
+			?>
+		</div>
+
+	</div>
+
+
+
+	<div class="main-column column">
+		
+		<form class="post-form" action="index.php" method="POST">
+			<textarea name="post-text" id="post-text" placeholder="Got something to say?"></textarea>
+			<input type="submit" name="post" id="post-button" value="Post" />
+			<hr />
+		</form>
+
 	</div>
 
 
