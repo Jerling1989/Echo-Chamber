5cbc6b34fb6ffcc8cad9770734be678b3d8f356d
Jacob <jerling1989@gmail.com>
2018-03-26 22:03:25 -0400

    creating isFriend function in User object

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -61,6 +61,19 @@
 			}
 		}
 
+		// CHECK IF USER IS FRIENDS WITH ANOTHER USER
+		public function isFriend($username_to_check) {
+			$usernameComma = ',' . $username_to_check . ',';
+
+			// CHECK IF USER IS IN FRIEND_ARRAY OR IF USER IS YOURSELF
+			if (strstr($this->user['friend_array'], $usernameComma) || $username_to_check == $this->user['username']) {
+
+				return true;
+			} else {
+				return false;
+			}
+		}
+
 	}
 
 ?>
