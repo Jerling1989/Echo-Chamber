bb21ea3e8cfd7adc6de2e4a16884fa2eb1b652b6
Jacob <jerling1989@gmail.com>
2018-03-26 20:08:59 -0400

    auto load more posts

--- index.php
+++ index.php
@@ -48,21 +48,22 @@
 			<hr />
 		</form>
 
-		<?php
-			$post = new Post($connection, $userLoggedIn);
-			$post->loadPostsFriends();
-		?>
+
+		<div class="posts_area"></div>
 
 		<img id="loading" src="assets/img/icons/loading.gif" />
 
 	</div>
 
-	<script type="text/javascript">
-		
-		var userLoggedIn = '<?php ehco $userLoggedIn; ?>';
 
+	<script>
+
+		// CREATE USERLOGGEDIN VARIABLE
+		var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+
+		// DOCUMENT READY FUNCTION
 		$(document).ready(function() {
-			// LOADING GIF
+			// SHOW LOADING GIF
 			$('#loading').show();
 
 			// ORIGINAL AJAX REQUEST FOR LOADING FIRST POSTS
@@ -70,8 +71,57 @@
 				url: 'includes/handlers/ajax_load_posts.php',
 				type: 'POST',
 				data: 'page=1&userLoggedIn=' + userLoggedIn,
-				cache: false,
-				
+				cache:false,
+
+				success: function(data) {
+					// HIDE LOADING GIF
+					$('#loading').hide();
+					// LOAD POSTS ONTO POSTS_AREA DIV
+					$('.posts_area').html(data);
+				}
+			});
+
+			// AUTO LOAD POSTS (INFINITE SCROLLING) FUNCTION
+			$(window).scroll(function() {
+				// POSTS_AREA DIV HEIGHT VARIABLE
+				var height = $('.posts_area').height();
+				// SCROLLTOP VARIABLE
+				var scroll_top = $(this).scrollTop();
+				// VARIABLE FOR NEXT PAGE (MORE POSTS)
+				var page = $('.posts_area').find('.next').val();
+				// VARIABLE FOR NO MORE POSTS
+				var noMorePosts = $('posts_area').find('.noMorePosts').val();
+
+				// CHECK IF THE PAGE IS SCROLLED TO THE BOTTOM OF POSTS_AREA DIV
+				// AND THERE ARE ALSO MORE POSTS
+				if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
+
+					// SHOW LOADING GIF
+					$('#loading').show();
+
+					// VARIABLE OF AJAX REQUEST FOR MORE POSTS
+					var ajaxReq = $.ajax({
+						url: 'includes/handlers/ajax_load_posts.php',
+						type: 'POST',
+						data: 'page=' + page + '&userLoggedIn=' + userLoggedIn,
+						cache:false,
+
+						success: function(response) {
+							// REMOVE CURRENT .NEXTPAGE
+							$('.posts_area').find('.nextPage').remove();
+							// REMOVE CURRENT NOMORE POSTS
+							$('.posts_area').find('.noMorePosts').remove();
+
+							// HIDE LOADING GIF
+							$('#loading').hide();
+							// LOAD POSTS ONTO POSTS_AREA DIV
+							$('.posts_area').append(response);
+						}
+					});
+
+
+				}
+
 			});
 
 		});
