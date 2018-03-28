15055252368d7415ace5698ea38aac3c845a441a
Jacob <jerling1989@gmail.com>
2018-03-28 16:18:48 -0400

    like button handler

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -235,7 +235,7 @@
 
 											<div class='newsfeedPostOptions'>
 												Comments($comments_check_num)&nbsp;&nbsp;&nbsp;
-
+												<iframe src='like.php?post_id=$id'></iframe>
 											</div>
 
 										</div>
