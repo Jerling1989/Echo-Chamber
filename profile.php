067f722baf1b2630f162ab31e7c1dc91659aab08
Jacob <jerling1989@gmail.com>
2018-03-30 21:18:41 -0400

    profile post modal

--- profile.php
+++ profile.php
@@ -95,6 +95,9 @@
 		</form>
 		<!-- END FRIEND REQUEST BUTTON FORM -->
 
+		<!-- POST SOMETHING MODAL TRIGGER -->
+		<input type="submit" class="deep-blue" data-toggle="modal" data-target="#post-modal" value="Post Something" />
+
 	</div>
 	<!-- END LEFT USER INFO PANEL -->
 
@@ -107,6 +110,47 @@
 		<?php echo $username; ?>
 
 	</div>
+
+
+
+	<!-- POST MODAL (.MODAL-LG) -->
+	<div class="modal fade" id="post-modal" tabindex="-1" role="dialog" aria-labelledby="postModalLabel" aria-hidden="true">
+	  <div class="modal-dialog modal-lg" role="document">
+	    <div class="modal-content">
+
+				<!-- MODAL HEADER -->
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel">Post Something!</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+
+				<!-- MODAL BODY -->
+	      <div class="modal-body">
+	        <p>This will appear on the user's profile page and also their newsfeed for your friends to see!</p>
+
+	        <form class="profile_post" action="" method="POST">
+	        	<div class="form-group">
+	        		<textarea class="form-control" name="post_body"></textarea>
+	        		<input type="hidden" name="user_from" value="<?php echo $userLoggedIn; ?>" />
+	        		<input type="hidden" name="user_to" value="<?php echo $username; ?>" />
+	        	</div>
+	        </form>
+	      </div>
+
+				<!-- MODAL FOOTER -->
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	        <button type="button" class="btn btn-primary">Post</button>
+	      </div>
+
+	    </div>
+	  </div>
+	</div>
+	<!-- END POST MODAL -->
+
+
 
 
 	</div>
