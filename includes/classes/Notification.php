2f6973a7418b7394b326342123feb2d96695d1d6
Jacob <jerling1989@gmail.com>
2018-04-07 17:28:09 -0400

    create insert notifications function

--- includes/classes/Notification.php
+++ includes/classes/Notification.php
@@ -13,7 +13,6 @@
 			$this->user_obj = new User($connection, $user);
 		}
 
-
 		// FUNCTION TO GET NUMBER OF NOTIFICATIONS
 		public function getUnreadNumber() {
 			// SET $USERLOGGEDIN VARIABLE
@@ -23,6 +22,56 @@
 			// RETURN NUMBER OF RESULTS FROM QUERY
 			return mysqli_num_rows($query);
 		}
+
+		// FUNCTION TO INSERT NOTIFICATIONS INTO NOTIFICATIONS TABLE
+		public function insertNotification($post_id, $user_to, $type) {
+			// SET $USERLOGGEDIN VARIABLE
+			$userLoggedIn = $this->user_obj->getUsername();
+			// GET FIRST AND LAST NAME OF USER LOGGED IN
+			$userLoggedInName = $this->user_obj->getFirstAndLastName();
+			// SET VARIABLE TO CURRENT TIME
+			$date_time = date('Y-m-d H:i:s');
+
+			// SWITCH STATEMENT FOR TYPES OF NOTIFICATIONS
+			switch ($type) {
+				case 'comment':
+					$message = $userLoggedInName . ' commented on your post';
+					break;
+				case 'like':
+					$message = $userLoggedInName . ' liked your post';
+					break;
+				case 'profile':
+					$message = $userLoggedInName . ' posted on your post';
+					break;
+				case 'comment_non_owner':
+					$message = $userLoggedInName . ' commented on a post you commented on';
+					break;
+				case 'profile_comment':
+					$message = $userLoggedInName . ' commented on your profile post';
+					break;
+			}
+
+			// LINK VARIABLE 
+			$link = 'post.php?id=' . $post_id;
+
+			// DATABASE QUERY (INSERT NOTIFICATON DATA INTO NOTIFICATIONS TABLE)
+			$insert_query = mysqli_query($this->connection, "INSERT INTO notifications VALUES('', '$user_to', '$userLoggedIn', '$message', '$link', '$date_time', 'no', 'no')");
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
