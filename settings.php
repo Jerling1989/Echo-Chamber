e910e54c3f014477eb07f07be6bdf65e877b9c98
Jacob <jerling1989@gmail.com>
2018-04-09 18:01:24 -0400

    user settings form

--- settings.php
+++ settings.php
@@ -15,6 +15,37 @@
 	<br />
 	<a href="upload.php">Upload New Profile Picture</a><br /><br /><br />
 
+	Modify the values and click 'Update Details'
+
+	<form action="settings.php" method="POST">
+		First Name: <input type="text" name="first_name" value="<?php echo $user['first_name']; ?>" />
+		<br />
+		Last Name: <input type="text" name="last_name" value="<?php echo $user['last_name']; ?>" />
+		<br />
+		Email: <input type="email" name="email" value="<?php echo $user['email']; ?>" />
+		<br />
+		Username: <input type="text" name="username" value="<?php echo $user['username']; ?>" />
+		<br />
+	</form>
+	<br />
+
+	<h4>Change Password</h4>
+	<form action="settings.php" method="POST">
+		Old Password: <input type="password" name="old_password" />
+		<br />
+		New Password: <input type="password" name="new_password_1" />
+		<br />
+		Confirm New Password: <input type="password" name="new_password_2" />
+		<br />
+	</form>
+	<br />
+
+	<h4>Close Account</h4>
+	<form action="settings.php">
+		<input type="submit" name="close_account" id="close_account" value="Close Account" />
+	</form>
+	<br />
+
 </div>
 
 
