b1f4a92da7369f98baf0ddd212a8f4e53cfae2e8
Jacob <jerling1989@gmail.com>
2018-04-09 17:44:13 -0400

    create settings page

---
+++ settings.php
@@ -0,0 +1,27 @@
+<?php
+
+	// INCLUDE NECCESSARY FILES AND SCRIPTS
+	include('includes/header.php');
+	// include('includes/settings_handler.php');
+
+?>
+
+<div class="main-column column">
+	
+	<h4>Account Settings</h4>
+	<?php
+		echo '<img src="'.$user['profile_pic'].'" id="small_profile_pics" />';
+	?>
+	<br />
+	<a href="upload.php">Upload New Profile Picture</a><br /><br /><br />
+
+</div>
+
+
+
+
+
+</div>
+<!-- END WRAPPER DIV -->
+</body>
+</html>
