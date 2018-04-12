c5e333606e13bc3ce40a738f71dd8a783add8fb8
Jacob <jerling1989@gmail.com>
2018-04-11 20:14:34 -0400

    updatad settings_handler check for username

--- includes/form_handlers/settings_handler.php
+++ includes/form_handlers/settings_handler.php
@@ -23,38 +23,40 @@
 		$row = mysqli_fetch_array($email_check);
 		$matched_user = $row['username'];
 
+		// DATABASE QUERY TO CHECK IF USERNAME IS TAKEN
+		$username_check = mysqli_query($connection, "SELECT * FROM users WHERE username='$username'");
+		$username_row = mysqli_fetch_array($username_check);
+		$matched_username = $username_row['username'];
+
 		// IF EMAIL IS NOT TAKEN OR ALREADY BELONGS TO LOGGED IN USER
 		if ($matched_user == '' || $matched_user == $userLoggedIn) {
-			// CREATE UPDATED DETAILS MESSAGE
-			$message = 'Details Updated!<br /><br />';
-			// DATABASE QUERY (UPDATE USER'S INFO)
-			$query = mysqli_query($connection, "UPDATE users SET first_name='$first_name', last_name='$last_name', email='$email', username='$username' WHERE username='$userLoggedIn'");
 
-			// DATABASE QUERY (SELECT USER INFO FROM USER EMAIL)
-			$new_username_query = mysqli_query($connection, "SELECT * FROM users WHERE email='$email'");
-			$row = mysqli_fetch_array($new_username_query);
-			// CREATE NEW USERNAME VARIABLE
-			$new_username = $row['username'];
+			// IF USERNAME IS NOT TAKEN OR ALREADY BELONGS TO LOGGED IN USER
+			if ($matched_username == '' || $matched_username == $userLoggedIn) {
 
-			// IF NEW USERNAME VARIABLE DOES NOT MATCH CURRENT $USERLOGGEDIN VARIABLE
-			if ($new_username != $userLoggedIn) {
+				// CREATE UPDATED DETAILS MESSAGE
+				$message = 'Details Updated!<br /><br />';
+
+				// UPDATE USER INFO IN USERS TABLE
+				$query = mysqli_query($connection, "UPDATE users SET first_name='$first_name', last_name='$last_name', email='$email', username='$username' WHERE username='$userLoggedIn'");
+
 				// UPDATE USERNAME IN POSTS TABLE
-				$update_posts_one = mysqli_query($connection, "UPDATE posts SET added_by='$new_username' WHERE added_by='$userLoggedIn'");
-				$update_posts_two = mysqli_query($connection, "UPDATE posts SET user_to='$new_username' WHERE user_to='$userLoggedIn'");
+				$update_posts_one = mysqli_query($connection, "UPDATE posts SET added_by='$username' WHERE added_by='$userLoggedIn'");
+				$update_posts_two = mysqli_query($connection, "UPDATE posts SET user_to='$username' WHERE user_to='$userLoggedIn'");
 				// UPDATE USERNAME IN NOTIFICATIONS TABLE
-				$update_notifications_one = mysqli_query($connection, "UPDATE notifications SET user_from='$new_username' WHERE user_from='$userLoggedIn'");
-				$update_notifications_two = mysqli_query($connection, "UPDATE notifications SET user_to='$new_username' WHERE user_to='$userLoggedIn'");
+				$update_notifications_one = mysqli_query($connection, "UPDATE notifications SET user_from='$username' WHERE user_from='$userLoggedIn'");
+				$update_notifications_two = mysqli_query($connection, "UPDATE notifications SET user_to='$username' WHERE user_to='$userLoggedIn'");
 				// UPDATE USERNAME IN MESSAGES TABLE
-				$update_messages_one = mysqli_query($connection, "UPDATE messages SET user_from='$new_username' WHERE user_from='$userLoggedIn'");
-				$update_messages_two = mysqli_query($connection, "UPDATE messages SET user_to='$new_username' WHERE user_to='$userLoggedIn'");
+				$update_messages_one = mysqli_query($connection, "UPDATE messages SET user_from='$username' WHERE user_from='$userLoggedIn'");
+				$update_messages_two = mysqli_query($connection, "UPDATE messages SET user_to='$username' WHERE user_to='$userLoggedIn'");
 				// UPDATE USERNAME IN LIKES TABLE
-				$update_likes = mysqli_query($connection, "UPDATE likes SET username='$new_username' WHERE username='$userLoggedIn'");
+				$update_likes = mysqli_query($connection, "UPDATE likes SET username='$username' WHERE username='$userLoggedIn'");
 				// UPDATE USERNAME IN FRIEND_REQUESTS TABLE
-				$update_requests_one = mysqli_query($connection, "UPDATE friend_requests SET user_from='$new_username' WHERE user_from='$userLoggedIn'");
-				$update_requests_two = mysqli_query($connection, "UPDATE friend_requests SET user_to='$new_username' WHERE user_to='$userLoggedIn'");
+				$update_requests_one = mysqli_query($connection, "UPDATE friend_requests SET user_from='$username' WHERE user_from='$userLoggedIn'");
+				$update_requests_two = mysqli_query($connection, "UPDATE friend_requests SET user_to='$username' WHERE user_to='$userLoggedIn'");
 				// UPDATE USERNAME IN COMMENTS TABLE
-				$update_comments_one = mysqli_query($connection, "UPDATE comments SET posted_by='$new_username' WHERE posted_by='$userLoggedIn'");
-				$update_comments_two = mysqli_query($connection, "UPDATE comments SET posted_to='$new_username' WHERE posted_to='$userLoggedIn'");
+				$update_comments_one = mysqli_query($connection, "UPDATE comments SET posted_by='$username' WHERE posted_by='$userLoggedIn'");
+				$update_comments_two = mysqli_query($connection, "UPDATE comments SET posted_to='$username' WHERE posted_to='$userLoggedIn'");
 
 				// FIND FRIEND ARRAYS WITH CURRENT $USERLOGGEDIN USERNAME
 				$friend_array_query = mysqli_query($connection, "SELECT * FROM users WHERE friend_array LIKE '%$userLoggedIn%' ORDER BY id DESC");
@@ -65,15 +67,20 @@
 					$friend_array_username = $friend_array_row['username'];
 					$current_friend_array = $friend_array_row['friend_array'];
 					// REPLACE AND UPDATE USERNAME IN FRIEND ARRAYS
-					$new_friend_array = str_replace($userLoggedIn.',', $new_username.',', $current_friend_array);
+					$new_friend_array = str_replace($userLoggedIn.',', $username.',', $current_friend_array);
 					$update_friend_array = mysqli_query($connection, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$friend_array_username'");
 
 				}
 
 				// REPLACE $USERLOGGEDIN AND $_SESSION['USERNAME']
 				// VARIABLES WITH NEW USERNAME VALUE
-				$userLoggedIn = $new_username;
-				$_SESSION['username'] = $new_username;
+				$userLoggedIn = $username;
+				$_SESSION['username'] = $username;
+
+
+				// IF USERNAME IS ALREADY TAKEN BY SOMEBODY ELSE
+			} else {
+				$message = 'That username is already in use!<br /><br />';
 			}
 
 			// IF EMAIL IS ALREADY TAKEN BY SOMEBODY ELSE
@@ -101,7 +108,7 @@
 		$db_email = $row['email'];
 
 		// IF THE OLD PASSWORD MATCHES PASSWORD IN DATABASE
-		if (md5($old_password) == $db_password) {
+		if (md5(md5($db_email).$old_password) == $db_password) {
 			// IF THE TWO NEW PASSWORDS MATCH
 			if ($new_password_1 == $new_password_2) {
 				// IF THE TWO NEW PASSWORD ARE NOT PROPER LENGTH DISPLAY ERROR
@@ -110,7 +117,7 @@
 					// IF TWO NEW PASSWORDS ARE PROPER LENGTH
 				} else {
 					// ENCRYPT NEW PASSWORD (ADD EMAIL SALT LATER)
-					$new_password_md5 = md5($new_password_1);
+					$new_password_md5 = md5(md5($db_email).$new_password_1);
 					// DATABASE QUERY (UPDATE NEW PASSWORD)
 					$password_query = mysqli_query($connection, "UPDATE users SET password='$new_password_md5' WHERE username='$userLoggedIn'");
 					$password_message = 'Password has been changed<br /><br />';
