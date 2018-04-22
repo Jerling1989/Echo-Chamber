b88b95a53c1efabbf5f56dda541bab8165c13161
Jacob <jerling1989@gmail.com>
2018-04-22 03:59:52 -0400

    new ajax_load_notifications

--- includes/handlers/ajax_load_notifications.php
+++ includes/handlers/ajax_load_notifications.php
@@ -5,8 +5,7 @@
 	include('../classes/User.php');
 	include('../classes/Notification.php');
 
-
-	$limit = 7; // NUMBER OF MESSAGES TO LOAD
+	$limit = 8; // NUMBER OF MESSAGES TO LOAD
 
 	// CREATE NEW MESSAGE OBJECT
 	$notification = new Notification($connection, $_REQUEST['userLoggedIn']);
