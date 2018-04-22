e9194d6a12271b267074e06685c13bff233e6dfa
Jacob <jerling1989@gmail.com>
2018-04-22 04:40:23 -0400

    new user_closed

--- user_closed.php
+++ user_closed.php
@@ -1,17 +1,27 @@
 <?php
-
 	// INCLUDE NECCESSARY FILES AND SCRIPTS
 	include('includes/header.php');
-
 ?>
 
-<div class="main-column column" id="main-column">
-	<h4>User Closed</h4>
-
-	This account is closed.
-	<a href="index.php">Click here to go back.</a>
+<!-- USER CLOSED PANEL -->
+<div class="column col-xl-10 col-lg-12 col-md-12 text-center" id="closed-account-panel">
+	<!-- ACCOUNT CLOSED TITLE -->
+	<div class="col-md-12 text-center">
+		<h2>User Account Closed</h2>
+	</div>
+	<hr />
+	<br />
+	<!-- USER CLOSED ICON -->
+	<i class="fas fa-ban fa-10x"></i>
+	<br />
+	<br />
+	<br />
+	<!-- ACCOUNT CLOSED INFO & LINK BACK TO NEWSFEED -->
+	<p>This account is closed. <a href="index.php">Click here to go back.</a></p>
+	<br />
 </div>
 
-
+</div>
+<!-- END WRAPPER DIV -->
 </body>
 </html>
