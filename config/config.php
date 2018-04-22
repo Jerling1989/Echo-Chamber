0c03b0b11cd890384dd281a1a485c0c9a1c70ace
Jacob <jerling1989@gmail.com>
2018-04-22 06:26:04 -0400

    update config file

--- config/config.php
+++ config/config.php
@@ -1,8 +1,13 @@
 <?php
 
+	// CREATE CONNECTION VARIABLES
+	$servername = 'kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+	$username = 'eft7l9l9x0wp4xbq';
+	$password = 'qmuz44u8it1qzuvd';
+	$dbname = 'rdewxrnxjxxfdwfq';
+
 	// TURN ON OUTPUT BUFFERING
 	ob_start();
-
 	// START SESSION
 	session_start();
 
@@ -10,7 +15,7 @@
 	$timezone = date_default_timezone_set('America/New_York');
 
 	// CONNECTTION VARIABLE
-	$connection = mysqli_connect('localhost', 'root', 'root', 'echo_chamber_db');
+	$connection = mysqli_connect($servername, $username, $password, $dbname);
 
 	// CONNECTION ERROR
 	if(mysqli_connect_errno()) {
