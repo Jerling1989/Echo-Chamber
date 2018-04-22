784110e9f35220c9c14b4cbd5b52905be0a4bb77
Jacob <jerling1989@gmail.com>
2018-04-22 04:13:49 -0400

    new index

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 	// INCLUDE NECCESSARY FILES AND SCRIPTS
 	include('includes/header.php');
 
@@ -40,7 +39,6 @@
 					$uploadOk = 0;
 				}
 			}
-
 		}
 
 		// IF EVERYTHING WORKS ACCORDINGLY
@@ -56,169 +54,190 @@
 							'.$errorMessage.'
 						</div>';
 		}
-
 	}
-
 ?>
-	
-	<!-- USER DETAILS PANEL -->
-	<div class="user-details column">
 
-		<!-- PROFILE PIC AND LINK -->
-		<a href="<?php echo $userLoggedIn; ?>">
-			<img src="<?php echo $user['profile_pic']; ?>" />
-		</a>
+<!-- SCROLL TO TOP BUTTON -->
+<a href="#top-top" data-offset="100">
+	<button type="button" id="new-post" class="btn btn-outline-light">
+		<i class="fas fa-chevron-up fa-lg"></i>
+	</button>
+</a>
+<!-- END SCROLL TO TOP BUTTON -->
 
-		<!-- NAME, POSTS, & LIKES -->
-		<div class="user-details-left-right">
-			<a href="<?php echo $userLoggedIn; ?>">
+<!-- START GRID ROW FOR PAGE LAYOUT -->
+<div class="row">
+	<!-- LEFT COLUMN (USER INFO AND TRENDING) -->
+	<div class="col-lg-4 col-md-0">
+		<!-- USER DETAILS PANEL -->
+		<div class="column" id="user-details">
+			<div class="row">	
+				<div class="col-lg-6 text-center">
+					<!-- PROFILE PIC AND LINK -->
+					<a href="<?php echo $userLoggedIn; ?>">
+						<img class="img-fluid" src="<?php echo $user['profile_pic']; ?>" />
+					</a>
+				</div>
+				<div class="col-lg-6">
+					<!-- NAME, POSTS, & LIKES -->
+					<div style="padding-top: 10px;">
+						<a href="<?php echo $userLoggedIn; ?>">
+							<?php
+								echo '<span class="user-links">'.$user['first_name'].' '.$user['last_name'].'</span><br />';
+								echo '<span class="user-links">'.$user['username'].'</span>';
+							?>
+						</a>
+						<br /><br />
+						<?php
+							echo '<span class="user-stats">Friends: '.$user_friends.'</span><br />';
+							echo '<span class="user-stats">Posts: '.$user['num_posts'].'</span><br />';
+							echo '<span class="user-stats">Likes: '.$user['num_likes'].'</span><br />';
+						?>
+					</div>
+					<!-- END NAME, POSTS, & LIKES -->
+				</div>
+			</div>
+		</div>
+		<!-- END USER DETAILS PANEL -->
+
+		<!-- TRENDING PANEL -->
+		<div class="column" id="trending">
+			<h4>Popular Trends</h4><br />
+			<div class="trends">
 				<?php
-					echo $user['first_name'] . ' ' . $user['last_name'] . '<br />';
-					echo $user['username'];
+					$query = mysqli_query($connection, "SELECT * FROM trends ORDER BY hits DESC LIMIT 9");
+
+					foreach ($query as $row) {
+						$word = $row['title'];
+						$word_dot = strlen($word) >= 14 ? '...' : '';
+
+						$trimmed_word = str_split($word, 14);
+						$trimmed_word = $trimmed_word[0];
+
+						echo '<div style="padding: 1px;">';
+						echo $trimmed_word . $word_dot;
+						echo '<br /></div><br />';
+					}
 				?>
-			</a>
-			<br /><br />
-			<?php
-				echo 'Posts: ' . $user['num_posts'] . '<br />';
-				echo 'Likes: ' . $user['num_likes'];
-			?>
+			</div>
 		</div>
-		<!-- END NAME, POSTS, & LIKES -->
-
+		<!-- END TRENDING PANEL -->
 	</div>
-	<!-- END USER DETAILS PANEL -->
+	<!-- END LEFT COLUMN (USER INFO AND TRENDING) -->
 
-
-	<!-- MAIN COLUMN (NEWSFEED) -->
-	<div class="main-column column">
-		
+	<!-- MAIN COLUMN (POST FORM AND NEWSFEED) -->
+	<div class="col-lg-8 col-md-12">
+		<!-- MAIN COLUMN (POST FORM) -->
+		<div class="column" id="post-panel">
 		<!-- POST FORM -->
-		<form class="post-form" action="index.php" method="POST" enctype="multipart/form-data">
-			<!-- POST IMAGE INPUT -->
-			<input type="file" name="fileToUpload" id="fileToUpload" />
-			<!-- POST TEXTAREA -->
-			<textarea name="post-text" id="post-text" placeholder="Got something to say?"></textarea>
-			<!-- POST SUBMIT BUTTON -->
-			<input type="submit" name="post" id="post-button" value="Post" />
-			<hr />
-		</form>
-		<!-- END POST FORM -->
-
-		<!-- DIV TO DISPLAY POSTS -->
-		<div class="posts_area"></div>
-
-		<!-- LOADING GIF -->
-		<div id="loading"><img src="assets/img/icons/loading.gif" /></div>
-		
-	</div>
-	<!-- END MAIN COLUMN (NEWSFEED) -->
-
-
-	<!-- TRENDING PANEL -->
-	<div class="user-details column">
-		<h4>Popular</h4><br />
-
-		<div class="trends">
-			<?php
-
-				$query = mysqli_query($connection, "SELECT * FROM trends ORDER BY hits DESC LIMIT 9");
-
-				foreach ($query as $row) {
-					$word = $row['title'];
-					$word_dot = strlen($word) >= 14 ? '...' : '';
-
-					$trimmed_word = str_split($word, 14);
-					$trimmed_word = $trimmed_word[0];
-
-					echo '<div style="padding: 1px;">';
-					echo $trimmed_word . $word_dot;
-					echo '<br /></div><br />';
-				}
-
-			?>
+			<form class="post-form" action="index.php" method="POST" enctype="multipart/form-data">
+				<div class="row">
+					<div class="col-lg-9 col-sm-9 col-9">
+						<!-- POST TEXTAREA -->
+						<textarea name="post-text" id="post-text" placeholder="Got something to say?"></textarea>
+					</div>
+					<div class="col-lg-3 col-sm-3 col-3">
+						<!-- POST SUBMIT BUTTON -->
+						<input type="submit" class="btn btn-success" name="post" id="post-button" value="Post" />
+						<!-- POST IMAGE INPUT -->
+						<input type="file" name="fileToUpload" id="fileToUpload" class="inputfile" data-multiple-caption="{count} files selected" multiple />
+						<label id="uploadImage" for="fileToUpload" class="text-center">
+							<img src="assets/img/icons/image-icon.png">
+							<p id="add-image">Add Image</p>
+							<small><span style="color: #F7F8F9;"></span></small>
+						</label>
+					</div>
+				</div>
+				<br />
+			</form>
+			<!-- END POST FORM -->
 		</div>
+		<!-- END MAIN COLUMN (POST FORM) -->
+
+		<!-- MAIN COLUMN (NEWSFEED) -->
+		<div class="column" id="newsfeed-panel">		
+			<!-- DIV TO DISPLAY POSTS -->
+			<div class="posts_area"></div>
+			<!-- LOADING GIF -->
+			<div id="loading"><img src="assets/img/icons/loading.gif" /></div>
+		</div>
+		<!-- END MAIN COLUMN (NEWSFEED) -->
 	</div>
-	<!-- END TRENDING PANEL -->
+	<!-- END MAIN COLUMN (POST FORM AND NEWSFEED) -->
+</div>
+<!-- END ROW GRID ROW FOR PAGE LAYOUT -->
 
 
-	<!-- POST LOADING SCRIPT (NEWSFEED) -->
-	<script>
+<!-- POST LOADING SCRIPT (NEWSFEED) -->
+<script>
+	// CREATE USERLOGGEDIN VARIABLE
+	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
 
-		// CREATE USERLOGGEDIN VARIABLE
-		var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+	// DOCUMENT READY FUNCTION
+	$(document).ready(function() {
+		// SHOW LOADING GIF
+		$('#loading').show();
 
-		// DOCUMENT READY FUNCTION
-		$(document).ready(function() {
-			// SHOW LOADING GIF
-			$('#loading').show();
+		// ORIGINAL AJAX REQUEST FOR LOADING FIRST POSTS
+		$.ajax({
+			url: 'includes/handlers/ajax_load_posts.php',
+			type: 'POST',
+			data: 'page=1&userLoggedIn=' + userLoggedIn,
+			cache: false,
 
-			// ORIGINAL AJAX REQUEST FOR LOADING FIRST POSTS
-			$.ajax({
-				url: 'includes/handlers/ajax_load_posts.php',
-				type: 'POST',
-				data: 'page=1&userLoggedIn=' + userLoggedIn,
-				cache: false,
-
-				success: function(data) {
-					// HIDE LOADING GIF
-					$('#loading').hide();
-					// LOAD POSTS ONTO POSTS_AREA DIV
-					$('.posts_area').html(data);
-				}
-			});
-
-			// AUTO LOAD POSTS (INFINITE SCROLLING) FUNCTION
-			$(window).scroll(function() {
-				// POSTS_AREA DIV HEIGHT VARIABLE
-				var height = $('.posts_area').height();
-				// SCROLLTOP VARIABLE
-				var scroll_top = $(this).scrollTop();
-				// VARIABLE FOR NEXT PAGE (MORE POSTS)
-				var page = $('.posts_area').find('.nextPage').val();
-				// VARIABLE FOR NO MORE POSTS
-				var noMorePosts = $('.posts_area').find('.noMorePosts').val();
-
-				// CHECK IF THE PAGE IS SCROLLED TO THE BOTTOM OF POSTS_AREA DIV
-				// AND THERE ARE ALSO MORE POSTS
-				if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
-
-					// SHOW LOADING GIF
-					$('#loading').show();
-
-					// VARIABLE OF AJAX REQUEST FOR MORE POSTS
-					var ajaxReq = $.ajax({
-						url: 'includes/handlers/ajax_load_posts.php',
-						type: 'POST',
-						data: 'page=' + page + '&userLoggedIn=' + userLoggedIn,
-						cache:false,
-
-						success: function(response) {
-							// REMOVE CURRENT .NEXTPAGE
-							$('.posts_area').find('.nextPage').remove();
-							// REMOVE CURRENT NOMORE POSTS
-							$('.posts_area').find('.noMorePosts').remove();
-
-							// HIDE LOADING GIF
-							$('#loading').hide();
-							// LOAD POSTS ONTO POSTS_AREA DIV
-							$('.posts_area').append(response);
-						}
-					});
-
-
-				} // END IF
-
-				return false;
-
-			}); // END AUTO LOAD POSTS FUNCTION
-
+			success: function(data) {
+				// HIDE LOADING GIF
+				$('#loading').hide();
+				// LOAD POSTS ONTO POSTS_AREA DIV
+				$('.posts_area').html(data);
+			}
 		});
 
-	</script>
-	<!-- END POST LOADING SCRIPT (NEWSFEED) -->
+		// AUTO LOAD POSTS (INFINITE SCROLLING) FUNCTION
+		$(window).scroll(function() {
+			// POSTS_AREA DIV HEIGHT VARIABLE
+			var height = $('.posts_area').height();
+			// SCROLLTOP VARIABLE
+			var scroll_top = $(this).scrollTop();
+			// VARIABLE FOR NEXT PAGE (MORE POSTS)
+			var page = $('.posts_area').find('.nextPage').val();
+			// VARIABLE FOR NO MORE POSTS
+			var noMorePosts = $('.posts_area').find('.noMorePosts').val();
 
+			// CHECK IF THE PAGE IS SCROLLED TO THE BOTTOM OF POSTS_AREA DIV
+			// AND THERE ARE ALSO MORE POSTS
+			if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
 
-	</div>
-	<!-- END WRAPPER DIV -->
+				// SHOW LOADING GIF
+				$('#loading').show();
+
+				// VARIABLE OF AJAX REQUEST FOR MORE POSTS
+				var ajaxReq = $.ajax({
+					url: 'includes/handlers/ajax_load_posts.php',
+					type: 'POST',
+					data: 'page=' + page + '&userLoggedIn=' + userLoggedIn,
+					cache:false,
+
+					success: function(response) {
+						// REMOVE CURRENT .NEXTPAGE
+						$('.posts_area').find('.nextPage').remove();
+						// REMOVE CURRENT NOMORE POSTS
+						$('.posts_area').find('.noMorePosts').remove();
+
+						// HIDE LOADING GIF
+						$('#loading').hide();
+						// LOAD POSTS ONTO POSTS_AREA DIV
+						$('.posts_area').append(response);
+					}
+				});
+			} // END IF
+			return false;
+		}); // END AUTO LOAD POSTS FUNCTION
+	});
+</script>
+<!-- END POST LOADING SCRIPT (NEWSFEED) -->
+
+</div>
+<!-- END WRAPPER DIV -->
 </body>
 </html>
