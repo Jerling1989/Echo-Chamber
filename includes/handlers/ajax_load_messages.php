863c5fab3e8fe991160c850b58311fc5dd7bcb75
Jacob <jerling1989@gmail.com>
2018-04-22 03:59:02 -0400

    new ajax_load_messages

--- includes/handlers/ajax_load_messages.php
+++ includes/handlers/ajax_load_messages.php
@@ -5,12 +5,11 @@
 	include('../classes/User.php');
 	include('../classes/Message.php');
 
-
-	$limit = 7; // NUMBER OF MESSAGES TO LOAD
+	$limit = 8; // NUMBER OF MESSAGES TO LOAD
 
 	// CREATE NEW MESSAGE OBJECT
 	$message = new Message($connection, $_REQUEST['userLoggedIn']);
-	
-	echo $message->getConvosDropDown($_REQUEST, $limit);
+
+	echo $message->getConvosDropdown($_REQUEST, $limit);
 
 ?>
